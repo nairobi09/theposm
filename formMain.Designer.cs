543b44e77af3ca7166e504387e3c34e379252356
Nairobi09 <nairobiplan@gmail.com>
2023-04-18 01:40:33 +0900

    011

--- formMain.Designer.cs
+++ formMain.Designer.cs
@@ -29,39 +29,32 @@ namespace thepos
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnView = new System.Windows.Forms.Button();
-            this.lvOrder = new System.Windows.Forms.ListView();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
-            // btnView
+            // listView1
             // 
-            this.btnView.Location = new System.Drawing.Point(641, 104);
-            this.btnView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnView.Name = "btnView";
-            this.btnView.Size = new System.Drawing.Size(499, 222);
-            this.btnView.TabIndex = 0;
-            this.btnView.Text = "View";
-            this.btnView.UseVisualStyleBackColor = true;
-            this.btnView.Click += new System.EventHandler(this.btnView_Click);
-            // 
-            // lvOrder
-            // 
-            this.lvOrder.HideSelection = false;
-            this.lvOrder.Location = new System.Drawing.Point(3, 3);
-            this.lvOrder.Name = "lvOrder";
-            this.lvOrder.Size = new System.Drawing.Size(579, 857);
-            this.lvOrder.TabIndex = 1;
-            this.lvOrder.UseCompatibleStateImageBehavior = false;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(4, 93);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(494, 574);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // formMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1343, 968);
-            this.Controls.Add(this.lvOrder);
-            this.Controls.Add(this.btnView);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1262, 913);
+            this.ControlBox = false;
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "formMain";
+            this.ShowIcon = false;
             this.Text = "thepos";
             this.ResumeLayout(false);
 
@@ -69,8 +62,7 @@ namespace thepos
 
         #endregion
 
-        private System.Windows.Forms.Button btnView;
-        private System.Windows.Forms.ListView lvOrder;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
