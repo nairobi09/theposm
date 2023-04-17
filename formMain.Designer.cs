d119d5d7e9463a66df88f0cbba29f777623256a2
Nairo Tim <nairobiplan@gmail.com>
2023-04-17 13:32:04 +0900

    201

--- formMain.Designer.cs
+++ formMain.Designer.cs
@@ -30,25 +30,36 @@ namespace thepos
         private void InitializeComponent()
         {
             this.btnView = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnView
             // 
-            this.btnView.Location = new System.Drawing.Point(116, 167);
+            this.btnView.Location = new System.Drawing.Point(102, 134);
+            this.btnView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnView.Name = "btnView";
-            this.btnView.Size = new System.Drawing.Size(499, 223);
+            this.btnView.Size = new System.Drawing.Size(437, 178);
             this.btnView.TabIndex = 0;
             this.btnView.Text = "View";
             this.btnView.UseVisualStyleBackColor = true;
             this.btnView.Click += new System.EventHandler(this.btnView_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(291, 171);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // formMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1343, 967);
+            this.ClientSize = new System.Drawing.Size(1175, 774);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnView);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "formMain";
             this.Text = "thepos";
             this.ResumeLayout(false);
@@ -58,6 +69,7 @@ namespace thepos
         #endregion
 
         private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button button1;
     }
 }
 
