36981307ae0d1127791a650e33c46d28493836a1
Nairo Tim <nairobiplan@gmail.com>
2023-04-17 13:36:09 +0900

    203

--- formMain.Designer.cs
+++ formMain.Designer.cs
@@ -46,7 +46,7 @@ namespace thepos
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(169, 378);
+            this.button1.Location = new System.Drawing.Point(210, 415);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(291, 171);
             this.button1.TabIndex = 1;
