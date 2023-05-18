0a7b89f6df69535cd0edb12a13d11781305d92d5
Nairo Tim <nairobiplan@gmail.com>
2023-05-18 17:57:13 +0900

    9f 20230518

--- pay/frmPayCard.Designer.cs
+++ pay/frmPayCard.Designer.cs
@@ -29,20 +29,20 @@
         private void InitializeComponent()
         {
             this.panelback = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblInstall = new System.Windows.Forms.Label();
+            this.btnInstall00 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInstall03 = new System.Windows.Forms.Button();
+            this.btnKeyInput = new System.Windows.Forms.Button();
+            this.btnInstall06 = new System.Windows.Forms.Button();
+            this.btnInstall12 = new System.Windows.Forms.Button();
+            this.btmCardTemp = new System.Windows.Forms.Button();
+            this.btnCardRequest = new System.Windows.Forms.Button();
+            this.lblNetAmount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.btnClose = new System.Windows.Forms.Button();
             this.lblTitle = new System.Windows.Forms.Label();
-            this.btnCashRecept = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btmCashTemp = new System.Windows.Forms.Button();
-            this.btnKeyInput = new System.Windows.Forms.Button();
-            this.btn1t = new System.Windows.Forms.Button();
-            this.btn5t = new System.Windows.Forms.Button();
-            this.btn10t = new System.Windows.Forms.Button();
-            this.btn50t = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.panelback.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -52,9 +52,9 @@
             this.panelback.BackColor = System.Drawing.Color.WhiteSmoke;
             this.panelback.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelback.Controls.Add(this.groupBox1);
-            this.panelback.Controls.Add(this.btmCashTemp);
-            this.panelback.Controls.Add(this.btnCashRecept);
-            this.panelback.Controls.Add(this.label4);
+            this.panelback.Controls.Add(this.btmCardTemp);
+            this.panelback.Controls.Add(this.btnCardRequest);
+            this.panelback.Controls.Add(this.lblNetAmount);
             this.panelback.Controls.Add(this.label1);
             this.panelback.Controls.Add(this.btnClose);
             this.panelback.Controls.Add(this.lblTitle);
@@ -63,6 +63,140 @@
             this.panelback.Name = "panelback";
             this.panelback.Size = new System.Drawing.Size(523, 698);
             this.panelback.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblInstall);
+            this.groupBox1.Controls.Add(this.btnInstall00);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnInstall03);
+            this.groupBox1.Controls.Add(this.btnKeyInput);
+            this.groupBox1.Controls.Add(this.btnInstall06);
+            this.groupBox1.Controls.Add(this.btnInstall12);
+            this.groupBox1.Location = new System.Drawing.Point(42, 194);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(455, 134);
+            this.groupBox1.TabIndex = 56;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblInstall
+            // 
+            this.lblInstall.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblInstall.Location = new System.Drawing.Point(137, 26);
+            this.lblInstall.Margin = new System.Windows.Forms.Padding(0);
+            this.lblInstall.Name = "lblInstall";
+            this.lblInstall.Padding = new System.Windows.Forms.Padding(5);
+            this.lblInstall.Size = new System.Drawing.Size(162, 26);
+            this.lblInstall.TabIndex = 49;
+            this.lblInstall.Tag = "0";
+            this.lblInstall.Text = "0";
+            this.lblInstall.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnInstall00
+            // 
+            this.btnInstall00.Location = new System.Drawing.Point(136, 73);
+            this.btnInstall00.Name = "btnInstall00";
+            this.btnInstall00.Size = new System.Drawing.Size(90, 46);
+            this.btnInstall00.TabIndex = 52;
+            this.btnInstall00.Text = "일시불";
+            this.btnInstall00.UseVisualStyleBackColor = true;
+            this.btnInstall00.Click += new System.EventHandler(this.btnInstall00_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 14);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "⁜할부개월";
+            // 
+            // btnInstall03
+            // 
+            this.btnInstall03.Location = new System.Drawing.Point(228, 73);
+            this.btnInstall03.Name = "btnInstall03";
+            this.btnInstall03.Size = new System.Drawing.Size(61, 46);
+            this.btnInstall03.TabIndex = 53;
+            this.btnInstall03.Text = "3개월";
+            this.btnInstall03.UseVisualStyleBackColor = true;
+            this.btnInstall03.Click += new System.EventHandler(this.btnInstall03_Click);
+            // 
+            // btnKeyInput
+            // 
+            this.btnKeyInput.BackColor = System.Drawing.Color.Gray;
+            this.btnKeyInput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKeyInput.ForeColor = System.Drawing.Color.White;
+            this.btnKeyInput.Location = new System.Drawing.Point(331, 20);
+            this.btnKeyInput.Name = "btnKeyInput";
+            this.btnKeyInput.Size = new System.Drawing.Size(100, 40);
+            this.btnKeyInput.TabIndex = 51;
+            this.btnKeyInput.Text = "키입력";
+            this.btnKeyInput.UseVisualStyleBackColor = false;
+            this.btnKeyInput.Click += new System.EventHandler(this.btnKeyInput_Click);
+            // 
+            // btnInstall06
+            // 
+            this.btnInstall06.Location = new System.Drawing.Point(291, 73);
+            this.btnInstall06.Name = "btnInstall06";
+            this.btnInstall06.Size = new System.Drawing.Size(61, 46);
+            this.btnInstall06.TabIndex = 54;
+            this.btnInstall06.Text = "6개월";
+            this.btnInstall06.UseVisualStyleBackColor = true;
+            this.btnInstall06.Click += new System.EventHandler(this.btnInstall06_Click);
+            // 
+            // btnInstall12
+            // 
+            this.btnInstall12.Location = new System.Drawing.Point(354, 73);
+            this.btnInstall12.Name = "btnInstall12";
+            this.btnInstall12.Size = new System.Drawing.Size(61, 46);
+            this.btnInstall12.TabIndex = 55;
+            this.btnInstall12.Text = "12개월";
+            this.btnInstall12.UseVisualStyleBackColor = true;
+            this.btnInstall12.Click += new System.EventHandler(this.btnInstall12_Click);
+            // 
+            // btmCardTemp
+            // 
+            this.btmCardTemp.Location = new System.Drawing.Point(193, 349);
+            this.btmCardTemp.Name = "btmCardTemp";
+            this.btmCardTemp.Size = new System.Drawing.Size(137, 57);
+            this.btmCardTemp.TabIndex = 50;
+            this.btmCardTemp.Text = "임의등록";
+            this.btmCardTemp.UseVisualStyleBackColor = true;
+            // 
+            // btnCardRequest
+            // 
+            this.btnCardRequest.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(87)))), ((int)(((byte)(96)))));
+            this.btnCardRequest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCardRequest.ForeColor = System.Drawing.Color.White;
+            this.btnCardRequest.Location = new System.Drawing.Point(336, 348);
+            this.btnCardRequest.Name = "btnCardRequest";
+            this.btnCardRequest.Size = new System.Drawing.Size(137, 57);
+            this.btnCardRequest.TabIndex = 47;
+            this.btnCardRequest.Text = "승인요청";
+            this.btnCardRequest.UseVisualStyleBackColor = false;
+            this.btnCardRequest.Click += new System.EventHandler(this.btnCardRequest_Click);
+            // 
+            // lblNetAmount
+            // 
+            this.lblNetAmount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNetAmount.Location = new System.Drawing.Point(179, 144);
+            this.lblNetAmount.Margin = new System.Windows.Forms.Padding(0);
+            this.lblNetAmount.Name = "lblNetAmount";
+            this.lblNetAmount.Padding = new System.Windows.Forms.Padding(5);
+            this.lblNetAmount.Size = new System.Drawing.Size(162, 26);
+            this.lblNetAmount.TabIndex = 49;
+            this.lblNetAmount.Tag = "0";
+            this.lblNetAmount.Text = "0";
+            this.lblNetAmount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 14);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "⁜결제대상금액";
             // 
             // btnClose
             // 
@@ -92,134 +226,6 @@
             this.lblTitle.Text = "카드결제";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // btnCashRecept
-            // 
-            this.btnCashRecept.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(87)))), ((int)(((byte)(96)))));
-            this.btnCashRecept.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCashRecept.ForeColor = System.Drawing.Color.White;
-            this.btnCashRecept.Location = new System.Drawing.Point(336, 348);
-            this.btnCashRecept.Name = "btnCashRecept";
-            this.btnCashRecept.Size = new System.Drawing.Size(137, 57);
-            this.btnCashRecept.TabIndex = 47;
-            this.btnCashRecept.Text = "승인요청";
-            this.btnCashRecept.UseVisualStyleBackColor = false;
-            // 
-            // label4
-            // 
-            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label4.Location = new System.Drawing.Point(179, 144);
-            this.label4.Margin = new System.Windows.Forms.Padding(0);
-            this.label4.Name = "label4";
-            this.label4.Padding = new System.Windows.Forms.Padding(5);
-            this.label4.Size = new System.Drawing.Size(162, 26);
-            this.label4.TabIndex = 49;
-            this.label4.Tag = "0";
-            this.label4.Text = "0";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(63, 151);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 14);
-            this.label1.TabIndex = 48;
-            this.label1.Text = "⁜결제대상금액";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 33);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 14);
-            this.label2.TabIndex = 48;
-            this.label2.Text = "⁜할부개월";
-            // 
-            // label3
-            // 
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Location = new System.Drawing.Point(137, 26);
-            this.label3.Margin = new System.Windows.Forms.Padding(0);
-            this.label3.Name = "label3";
-            this.label3.Padding = new System.Windows.Forms.Padding(5);
-            this.label3.Size = new System.Drawing.Size(162, 26);
-            this.label3.TabIndex = 49;
-            this.label3.Tag = "0";
-            this.label3.Text = "0";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // btmCashTemp
-            // 
-            this.btmCashTemp.Location = new System.Drawing.Point(193, 349);
-            this.btmCashTemp.Name = "btmCashTemp";
-            this.btmCashTemp.Size = new System.Drawing.Size(137, 57);
-            this.btmCashTemp.TabIndex = 50;
-            this.btmCashTemp.Text = "임의등록";
-            this.btmCashTemp.UseVisualStyleBackColor = true;
-            // 
-            // btnKeyInput
-            // 
-            this.btnKeyInput.BackColor = System.Drawing.Color.Gray;
-            this.btnKeyInput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnKeyInput.ForeColor = System.Drawing.Color.White;
-            this.btnKeyInput.Location = new System.Drawing.Point(331, 20);
-            this.btnKeyInput.Name = "btnKeyInput";
-            this.btnKeyInput.Size = new System.Drawing.Size(100, 40);
-            this.btnKeyInput.TabIndex = 51;
-            this.btnKeyInput.Text = "키입력";
-            this.btnKeyInput.UseVisualStyleBackColor = false;
-            // 
-            // btn1t
-            // 
-            this.btn1t.Location = new System.Drawing.Point(136, 73);
-            this.btn1t.Name = "btn1t";
-            this.btn1t.Size = new System.Drawing.Size(90, 46);
-            this.btn1t.TabIndex = 52;
-            this.btn1t.Text = "일시불";
-            this.btn1t.UseVisualStyleBackColor = true;
-            // 
-            // btn5t
-            // 
-            this.btn5t.Location = new System.Drawing.Point(228, 73);
-            this.btn5t.Name = "btn5t";
-            this.btn5t.Size = new System.Drawing.Size(61, 46);
-            this.btn5t.TabIndex = 53;
-            this.btn5t.Text = "3개월";
-            this.btn5t.UseVisualStyleBackColor = true;
-            // 
-            // btn10t
-            // 
-            this.btn10t.Location = new System.Drawing.Point(291, 73);
-            this.btn10t.Name = "btn10t";
-            this.btn10t.Size = new System.Drawing.Size(61, 46);
-            this.btn10t.TabIndex = 54;
-            this.btn10t.Text = "6개월";
-            this.btn10t.UseVisualStyleBackColor = true;
-            // 
-            // btn50t
-            // 
-            this.btn50t.Location = new System.Drawing.Point(354, 73);
-            this.btn50t.Name = "btn50t";
-            this.btn50t.Size = new System.Drawing.Size(61, 46);
-            this.btn50t.TabIndex = 55;
-            this.btn50t.Text = "12개월";
-            this.btn50t.UseVisualStyleBackColor = true;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.btn1t);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.btn5t);
-            this.groupBox1.Controls.Add(this.btnKeyInput);
-            this.groupBox1.Controls.Add(this.btn10t);
-            this.groupBox1.Controls.Add(this.btn50t);
-            this.groupBox1.Location = new System.Drawing.Point(42, 194);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(455, 134);
-            this.groupBox1.TabIndex = 56;
-            this.groupBox1.TabStop = false;
-            // 
             // frmPayCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
@@ -246,17 +252,17 @@
         private System.Windows.Forms.Panel panelback;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Button btnCashRecept;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCardRequest;
+        private System.Windows.Forms.Label lblInstall;
+        private System.Windows.Forms.Label lblNetAmount;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btmCashTemp;
+        private System.Windows.Forms.Button btmCardTemp;
         private System.Windows.Forms.Button btnKeyInput;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button btn1t;
-        private System.Windows.Forms.Button btn5t;
-        private System.Windows.Forms.Button btn10t;
-        private System.Windows.Forms.Button btn50t;
+        private System.Windows.Forms.Button btnInstall00;
+        private System.Windows.Forms.Button btnInstall03;
+        private System.Windows.Forms.Button btnInstall06;
+        private System.Windows.Forms.Button btnInstall12;
     }
 }
--- pay/frmPayCard.cs
+++ pay/frmPayCard.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,9 +14,109 @@ namespace thepos.pay
 {
     public partial class frmPayCard : Form
     {
+        [DllImport("C:\\TossPGPos\\TossPGPOSClient64.dll", EntryPoint = "UPay_Init", CallingConvention = CallingConvention.StdCall)]
+        extern static int UPay_Init();
+
+        [DllImport("C:\\TossPGPos\\TossPGPOSClient64.dll", EntryPoint = "UPay_Set", CallingConvention = CallingConvention.StdCall)]
+        extern static int UPay_Set(string name, string value);
+
+        [DllImport("C:\\TossPGPos\\TossPGPOSClient64.dll", EntryPoint = "UPay_TX", CallingConvention = CallingConvention.StdCall)]
+        extern static int UPay_TX();
+
+        [DllImport("C:\\TossPGPos\\TossPGPOSClient64.dll", EntryPoint = "UPayResNameCount", CallingConvention = CallingConvention.StdCall)]
+        extern static int UPayResNameCount();
+
+        [DllImport("C:\\TossPGPos\\TossPGPOSClient64.dll", EntryPoint = "UPayResName", CallingConvention = CallingConvention.StdCall)]
+        extern static string UPayResName(int index);
+
+        [DllImport("C:\\TossPGPos\\TossPGPOSClient64.dll", EntryPoint = "UPayResponse", CallingConvention = CallingConvention.StdCall)]
+        extern static string UPayResponse(int index);
+
+        [DllImport("C:\\TossPGPos\\TossPGPOSClient64.dll", EntryPoint = "UPayFinal", CallingConvention = CallingConvention.StdCall)]
+        extern static int UPayFinal();
+
         public frmPayCard()
         {
             InitializeComponent();
+
+            initial_the();
+
+
+        }
+
+        private void initial_the()
+        {
+            lblNetAmount.Text = mNetAmount.ToString("N0");
+        }
+
+
+
+
+        private void btnCardRequest_Click(object sender, EventArgs e)
+        {
+            int ret = UPay_Init();
+            if (ret == -9)
+            {
+                MessageBox.Show("Toss DLL 초기화 오류");
+                return;
+            }
+
+            ret = UPay_Set("LGD_TXNAME", "CardAuthOfflinePos");
+            ret = UPay_Set("LGD_REQTYPE", "APPR");
+            ret = UPay_Set("LGD_MID", "");
+            ret = UPay_Set("LGD_OID", "");
+            ret = UPay_Set("LGD_AMOUNT", mNetAmount.ToString());
+            ret = UPay_Set("LGD_INSTALL", lblInstall.Text);
+            ret = UPay_Set("LGD_TAXFREEAMOUNT", "0");
+            ret = UPay_Set("LGD_VAT", "0");
+            ret = UPay_Set("VAN_SFEEAMOUNT", "0");
+            ret = UPay_Set("VAN_TRANTYPE", "S0");
+
+
+
+
+
+
+
+
+            ret = UPay_TX();
+
+            int cnt = UPayResNameCount();
+
+            string Rname;
+            string Rvalue;
+
+            for (int i = 0; i < cnt; i++)
+            {
+                // 응답 메시지 리턴
+                Rname = UPayResName(i);
+                Rvalue = UPayResponse(i);
+
+                // 응답메시지 파싱
+                if (Rname == "Respcode") { string rRespcode = Rvalue; }
+
+            }
+            // TossPaymentsPOS_Client 자원반환
+            ret = UPayFinal();
+
+        }
+
+
+
+
+
+
+
+
+
+        private void btnInstall00_Click(object sender, EventArgs e) { lblInstall.Text = "00"; }
+        private void btnInstall03_Click(object sender, EventArgs e) { lblInstall.Text = "03"; }
+        private void btnInstall06_Click(object sender, EventArgs e) { lblInstall.Text = "06"; }
+        private void btnInstall12_Click(object sender, EventArgs e) { lblInstall.Text = "12"; }
+
+        private void btnKeyInput_Click(object sender, EventArgs e)
+        {
+            lblInstall.Text = mLblKeyDisplay.Text;
         }
 
         private void btnClose_Click(object sender, EventArgs e)
@@ -27,5 +128,7 @@ namespace thepos.pay
         {
             frmSale.ConsoleEnable();
         }
+
+
     }
 }
