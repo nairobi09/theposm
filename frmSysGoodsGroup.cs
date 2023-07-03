0414b579448fc221047896ed0a75111e0dbe1a8a
Nairobi09 <nairobiplan@gmail.com>
2023-07-03 22:54:24 +0900

    home 20230603

--- frmSysGoodsGroup.cs
+++ frmSysGoodsGroup.cs
@@ -10,7 +10,7 @@ namespace thepos
 {
     public partial class frmSysGoodsGroup : Form
     {
-        static readonly HttpClient httpClient = new HttpClient();
+        HttpClient httpClient = new HttpClient();
 
 
         public frmSysGoodsGroup()
@@ -23,52 +23,11 @@ namespace thepos
 
         private void btnView_Click(object sender, EventArgs e)
         {
-            string httpsUrl = "https://jsonplaceholder.typicode.com/todos/1";
-            string httpUrl = "http://jsonplaceholder.typicode.com/todos/2";
 
-            Console.WriteLine($" -------- HTTPS ------------");
-
-            Test(httpsUrl).GetAwaiter().GetResult();  // Main함수에서 await Test(httpsUrl) 사용못하므로, 이를 대신함
-
-            Console.WriteLine($"\n\n\n --------- HTTP ------------");
-
-            Test(httpUrl).GetAwaiter().GetResult();
-
-            Console.WriteLine($" ---------- END ------------");
-            Console.Read();
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("http://www.microsoft.com/");
+            string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
         }
 
-        static async Task Test(string url)
-        {
-            try
-            {
-                using (var response = await httpClient.GetAsync(url))
-                {
-                    Console.WriteLine(response.StatusCode);
 
-                    if (HttpStatusCode.OK == response.StatusCode)
-                    {
-                        string body = await response.Content.ReadAsStringAsync();
-                        Console.WriteLine(body);
-                    }
-                    else
-                    {
-                        Console.WriteLine($" -- response.ReasonPhrase ==> {response.ReasonPhrase}");
-                    }
-                }
-
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"ex.Message={ex.Message}");
-                Console.WriteLine($"ex.InnerException.Message = {ex.InnerException.Message}");
-
-                Console.WriteLine($"----------- 서버에 연결할수없습니다 ---------------------");
-            }
-            catch (Exception ex2)
-            {
-                Console.WriteLine($"Exception={ex2.Message}");
-            }
-        }
     }
 }
