14e881f54cd405e3ba8b0b6123c54dc247b4007e
Nairobi09 <nairobiplan@gmail.com>
2023-07-05 01:02:59 +0900

    h 20230704

--- frmSysGoodsGroup.cs
+++ frmSysGoodsGroup.cs
@@ -2,16 +2,19 @@
 
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace thepos
 {
     public partial class frmSysGoodsGroup : Form
     {
-        HttpClient httpClient = new HttpClient();
 
+        String URL = "http://211.42.156.219:8080/goods";
+        string urlParameters = "?siteId=sh01&posNo=01";
 
         public frmSysGoodsGroup()
         {
@@ -19,13 +22,30 @@ namespace thepos
         }
 
 
-
-
         private void btnView_Click(object sender, EventArgs e)
         {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
 
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("http://www.microsoft.com/");
-            string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonString = JsonSerializer.Serialize(response);
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            // Make any other calls using HttpClient here.
+
+            // Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
+            client.Dispose();
+
         }
 
 
